be3a2072e24b0e0c93ed5d26cb8b8af174277f16
Tarik Alic <tarikalic1207@gmail.com>
2025-02-15 17:17:53 +0100

    Dodavanje izmjena.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     manager = ProductManager()
     korpa = Cart()
     
-    # Dodavanje nekoliko proizvoda
     p1 = Product("Televizor", 120000, 3)
     p2 = Product("Laptop", 95000, 4)
     p3 = Product("Smartphone", 15000, 6)
     
+    # Dodavanje proizvoda
     manager.add_product(p1)
     manager.add_product(p2)
     manager.add_product(p3)
@@ -20,11 +20,10 @@ def main():
     korpa.add_product(p2)
     korpa.add_product(p3)
 
-    # Prikaz proizvoda i ukupne vrednosti
+    # Prikaz
     manager.display_products()
     manager.total_value()
     
-    # Prikaz sadržaja korpe i ukupnog iznosa
     korpa.show_cart()
     print(f"Ukupno za naplatu: {korpa.total_price():.2f}.")
 
